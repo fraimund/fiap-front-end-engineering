faf3329e976b41aa44747a2f49557e7b9144b0ab
fraimund <fraimund@its.jnj.com>
2025-03-04 13:38:18 -0300

    Create my first personal website

---
+++ components/ProfileInfo.vue
@@ -0,0 +1,24 @@
+<template>
+        <figure class="p-6">
+            <img
+                class="w-32 h-32 mx-auto rounded-full"
+                :src="photo"
+                alt="profile picture"
+                width="400"
+                height="400"
+            />
+            <div class="pt-6 text-center spacce-y-4">
+                <figcaption class="font-medium">
+                    <div class="text-cyan-900 text-xl">{{ title }}</div>
+                    <div class="text-gray-500 font-light">{{ subtitle }}</div>
+                </figcaption>
+                <blockquote>
+                    <p class="text-lg font-medium">{{ headline }}</p>
+                </blockquote>
+            </div>
+        </figure>
+</template>
+
+<script setup>
+    const { photo, title, subtitle, headline } = defineProps(['photo', 'title', 'subtitle', 'headline']);
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <ProfileInfo photo="/foto.jpg"
+                 title="Fábio A. Raimundo" 
+                 subtitle="fabio.ap.raimundo@gmail.com" 
+                 headline="Tech Manager | Data Enginer | Product Lead" />
+
+    <main class="max-w-xs mx-auto">
+        <ButtonAction link="https://www.linkedin.com/in/fabio-a-raimundo-842a8b14/"
+                      icon="👤"
+                      title="Linkedin"
+        />
+        <ButtonAction link="https://wa.me/5512996474944"
+                      icon="🗨"
+                      title="WhatsApp"
+        />
+        <ButtonAction link="https://www.instagram.com/fabio.a.raimundo/"
+                      icon="👨‍👩‍👧‍👦"
+                      title="Instagram"
+        />
+        <ButtonAction link="https://github.com/fraimund"
+                      icon="👨🏾‍💻"
+                      title="github"
+        />
+    </main>
+
+</template>
